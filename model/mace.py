85b4de34e55276e91e5c0d8f79360b66fd538933
hgwoops <113457887+hgwoops@users.noreply.github.com>
2026-04-17 18:02:33 +0800

    Delete model/mace.py

--- model/mace.py
+++
@@ -1,8 +0,0 @@
-"""
-Prism MACE model entrypoint.
-
-This clean Prism workspace reuses the MACE wrapper implementation from the active GeoMo codebase.
-"""
-
-from GeoMo.model.mace import *  # noqa: F401,F403
-
